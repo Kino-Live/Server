319a5fd92718d179ce764f94b8fb92b7ec120b31
NureKovalenkoVioletta <violetta.kovalenko@nure.ua>
2025-10-11 21:23:16 +0300

    modified user entity

--- ProjectCinema/Entities/User.cs
+++ ProjectCinema/Entities/User.cs
@@ -9,26 +9,39 @@ namespace ProjectCinema.Entities
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int UserId { get; set; }
+
         [Required]
         [StringLength(256, MinimumLength = 2)]
-        public string FirstName { get; set; }
+        public string FirstName { get; set; } = null!;
+
         [Required]
         [StringLength(256, MinimumLength = 2)]
-        public string LastName { get; set; }
+        public string LastName { get; set; } = null!;
+
         [Required]
         [StringLength(256, MinimumLength = 2)]
-        public string Username { get; set; }
+        public string Username { get; set; } = null!;
+
         [Required]
         [EmailAddress]
-        public string Email { get; set; }
+        public string Email { get; set; } = null!;
+
+        [Required]
+        public byte[] PasswordHash { get; set; } = null!;
+
         [Phone]
-        public string PhoneNumber { get; set; }
+        public string PhoneNumber { get; set; } = null!;
+
         [Required]
         public UserRole UserRole { get; set; }
 
         public DateOnly DateOfBirth { get; set; }
+
         [Required]
         public DateTime CreatedAt { get; set; }
-        public ICollection<Booking> Bookings { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+
+        public ICollection<Booking>? Bookings { get; set; }
     }
 }
