3e27b1c19f3ed600c431909f94af71b8af8425d8
NureKovalenkoVioletta <violetta.kovalenko@nure.ua>
2025-10-11 18:01:10 +0300

    modified showTime entity

--- ProjectCinema/Entities/ShowTime.cs
+++ ProjectCinema/Entities/ShowTime.cs
@@ -9,25 +9,37 @@ namespace ProjectCinema.Entities
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ShowTimeId { get; set; }
+
         [Required]
         public DateTime StartTime { get; set; }
+
         [Required]
         public DateTime EndTime { get; set; }
+
         [Required]
         public ViewingFormat ViewingFormat { get; set; }
+
         public ShowTimeStatus ShowTimeStatus { get; set; }
+        
         [Required]
         [Range(1, double.MaxValue)]
-        public Decimal TicketPrice { get; set; }
+        public decimal TicketPrice { get; set; }
+
         [Required]
         public DateTime CreatedAt { get; set; }
-        public ICollection<Ticket> Tickets { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+        public ICollection<Ticket>? Tickets { get; set; }
+
         [ForeignKey("MovieScreeningId")]
-        public MovieScreening MovieScreening { get; set; }
+        public MovieScreening? MovieScreening { get; set; }
+
         [Required]
         public int MovieScreeningId { get; set; }
+
         [ForeignKey("HallId")]
-        public Hall Hall { get; set; }
+        public Hall? Hall { get; set; }
+
         [Required]
         public int HallId { get; set; }
         
