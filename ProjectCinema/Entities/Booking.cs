91dc74ffd4f8842a3a148dd79c48e1363fb463e1
NureKovalenkoVioletta <violetta.kovalenko@nure.ua>
2025-10-11 18:37:42 +0300

    modified booking entity

--- ProjectCinema/Entities/Booking.cs
+++ ProjectCinema/Entities/Booking.cs
@@ -9,22 +9,32 @@ namespace ProjectCinema.Entities
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int BookingId { get; set; }
+
         [Required]
-        public Decimal TotalPrice { get; set; }
+        public decimal TotalPrice { get; set; }
+
         [Required]
         public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+
         [Required]
         public BookingStatus BookingStatus { get; set; }
+
         [ForeignKey("UserId")]
-        public User User { get; set; }
+        public User? User { get; set; }
         [Required]
         public int UserId { get; set; }
+
         [ForeignKey("PeymentId")]
-        public Payment Payment { get; set; }
+        public Payment? Payment { get; set; }
+
         [Required]
         public int PaymentId { get; set; }
+
         [ForeignKey("PromocodeId")]
-        public Promocode Promocode { get; set; }
+        public Promocode? Promocode { get; set; }
+
         [Required]
         public int PromocodeId { get; set; }
         public ICollection<Ticket>? Tickets { get; set; }
