d1593493e3e5449ae448f9a63c496c6790726181
NureKovalenkoVioletta <violetta.kovalenko@nure.ua>
2025-10-11 17:46:22 +0300

    modified row entity

--- ProjectCinema/Entities/Row.cs
+++ ProjectCinema/Entities/Row.cs
@@ -8,13 +8,21 @@ namespace ProjectCinema.Entities
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int RowId { get; set; }
-        [Required]
-        public int HallId { get; set; }
-        [ForeignKey("HallId")]
-        public Hall? Hall { get; set; }
+
         [Required]
         public int RowNumber { get; set; }
 
+        [Required]
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+        [Required]
+        public int HallId { get; set; }
+
+        [ForeignKey("HallId")]
+        public Hall? Hall { get; set; }
+
+        [Required]
         public ICollection<Seat>? Seats { get; set; }
     }
 }
